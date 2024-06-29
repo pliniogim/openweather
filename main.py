eb934781bd900b924b5c1057c9bbca63f24762ca
pliniogim <pliniogimenez@gmail.com>
2024-06-29 14:42:48 -0300

    final

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import key
 import requests
-import json
+from twilio.rest import Client
 
-response = requests.get(url=key.URL_FORECAST)
-data = json.loads(response.text)
-print(data)
+response = requests.get(url=key.URL_FORECAST, params=key.ow_params)
+response.raise_for_status()
+ow_data = response.json()
+
+will_rain = False
+
+for hour_data in ow_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) <= 800:
+        will_rain = True
+
+if will_rain:
+    client = Client(key.TW_ACCOUNT_SID, key.TW_AUTH)
+    message = client.messages.create(from_=key.TW_PHONE,
+                                     to=key.MY_OWN_PHONE,
+                                     body="Rain coming!")
+    print(message.sid)
